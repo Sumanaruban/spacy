20edaea88397f8a6a34c3e17680ddd719298de95
sumanaruban <sumanaruban@users.noreply.github.com>
2022-05-21 16:37:44 +0530

    add multi_lingual_NER function

--- spacy_q14.py
+++ spacy_q14.py
@@ -1,2 +1,13 @@
 # Sumani
 # [21-5-2022]
+
+import spacy
+
+def multi_lingual_NER(text, lang_code):
+    ret = []
+    nlp = spacy.load(lang_code)
+    doc = nlp(text)
+    for token in doc:
+        d = {'text':token.text, 'type':token.type, 'start_pos':token.idx, 'end_pos':token.idx+len(token.text)}
+        ret.append(d)
+    return ret   
